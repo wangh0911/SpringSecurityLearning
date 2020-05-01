d9f091a45354e77f32207c0e8dc4c01f07f33e4c
wanghao0911 <1011954307@qq.com>
2020-05-01 23:21:02 +0800

    initial commit

---
+++ src/main/java/cn/roboteco/springsecuritylearning/SpringSecurityLearningApplication.java
@@ -0,0 +1,21 @@
+package cn.roboteco.springsecuritylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringSecurityLearningApplication {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello,spring security";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/roboteco/springsecuritylearning/SpringSecurityLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.roboteco.springsecuritylearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
